1185c3ad498e7fd901d74d3acbf1f91e55e06bcf
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-11-10 18:30:36 +0100

    Paginated list of products in admin

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,51 @@
-import Heading from "@/components/ui/Heading";
+import ProductsPagination from "@/components/products/ProductsPagination"
+import ProductTable from "@/components/products/ProductsTable"
+import Heading from "@/components/ui/Heading"
+import { prisma } from "@/src/lib/prisma"
+import { redirect } from "next/navigation"
 
-export default function ProductsPage() {
+async function productCount() {
+    return await prisma.product.count()
+}
+
+async function getProducts(page: number, pageSize: number) {
+    const skip = (page - 1) * pageSize
+    return await prisma.product.findMany({
+        take: pageSize,
+        skip: skip,
+        include: {
+            category: true
+        }
+    })
+}
+
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
+
+export default async function ProductsPage(props: { searchParams: Promise<{ page: string }> }) {
+    const searchParams = await props.searchParams;
+
+    const page = await +searchParams.page || 1
+    const pageSize = 10
+
+    if (page < 0) redirect("/admin/products")
+
+    const produtsData = await getProducts(page, pageSize)
+    const totalProductsData = await productCount()
+    const [produts, totalProducts] = await Promise.all([produtsData, totalProductsData])
+    const totalPages = Math.ceil(totalProducts / pageSize)
+
+    if (page > totalPages) redirect("/admin/products")
+
     return (
         <>
             <Heading>Manage Products</Heading>
+            <ProductTable
+                products={produts}
+            />
+            <ProductsPagination
+                page={page}
+                totalPages={totalPages}
+            />
         </>
     )
 }
